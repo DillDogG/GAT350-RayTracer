ce3141e462a8665b2bbfb01c5d97a8a14c91d3f8
DillDogG <dilldoggaming@gmail.com>
2023-11-29 18:02:37 -0700

    Color

---
+++ Source/RayTracer/Color.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <../glm/glm/glm.hpp>
+
+using color3_t = glm::vec3;
+using color4_t = glm::vec4;
+using rgba_t = glm::uint32_t;
+
+//inline color4_t RGBAToColor(const rgba_t* rgba) {
+//	return color4_t{ rgba / 255 };
+//}
